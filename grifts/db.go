0329db2d0413b870ae77de296f1a44db1fbab1c3
BalmanRawat <balman.rawat@gmail.com>
2019-03-20 23:18:20 +0545

    very raw data and csv seeder

--- grifts/db.go
+++ grifts/db.go
@@ -2,6 +2,13 @@ package grifts
 
 import (
 	"github.com/markbates/grift/grift"
+	"github.com/balmanrawat/kopila-api/models"
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
 var _ = grift.Namespace("db", func() {
@@ -9,6 +16,31 @@ var _ = grift.Namespace("db", func() {
 	grift.Desc("seed", "Seeds a database")
 	grift.Add("seed", func(c *grift.Context) error {
 		// Add DB seeding stuff here
+		
+		csvFile, err := os.Open("grifts/data.csv")
+		if err != nil {
+			log.Fatal(err)
+		}
+		
+		reader := csv.NewReader(bufio.NewReader(csvFile))
+		for {
+			line, error := reader.Read()
+			if error == io.EOF {
+				break
+			} else if error != nil {
+				log.Fatal(error)
+			}
+			fmt.Println(line)
+			u := &models.User{
+				Name: line[0],
+				PhoneNumber: line[1],
+				Tole: line[2],
+				Ward: line[3],
+			}
+
+			fmt.Println(u)
+			models.DB.Create(u)
+		}
 		return nil
 	})
 
